06a8afc966d5b1b90265b62db8a9694a0f7965fb
Joyang <87349221+jackyang1989@users.noreply.github.com>
2025-11-19 20:03:50 +0800

    Create init_db.py

---
+++ backend/app/db/init_db.py
@@ -0,0 +1,13 @@
+from backend.app.db.base_class import Base
+from backend.app.db.session import engine
+# 必须导入所有定义好的 Model，否则无法自动创建表
+from backend.app.models.user import User 
+
+def init_db():
+    print("正在创建数据库表...")
+    # 这行代码会检查数据库，如果表不存在，就根据 Model 自动创建
+    Base.metadata.create_all(bind=engine)
+    print("数据库表创建成功！")
+
+if __name__ == "__main__":
+    init_db()
